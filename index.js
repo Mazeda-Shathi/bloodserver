7504e6e8ece8cf28003cf3eca4828286a82e3b23
Mazeda-Shathi <mazedaaktershathi2019@gmail.com>
2022-01-01 11:16:55 +0600

    trial

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const cors = require('cors');
 app.use(cors());
-const port = 3001 || process.env.PORT;
+const port = process.env.PORT || 4000;
 app.use(express.json());
 
 
